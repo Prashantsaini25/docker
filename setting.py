ee2d7a6109c7ae22d6b1e016d5265c841f0d38c8
Prashantsaini25 <64362251+Prashantsaini25@users.noreply.github.com>
2020-04-26 18:43:25 +0530

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,11 @@
+#setting.py
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
